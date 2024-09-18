de84c153620bf5cf9f55ef3a75a90b7acfffd286
fryingDog <kzhr2011aug@gmail.com>
2024-09-18 20:08:56 +0900

    Create script.js
    基本

---
+++ script.js
@@ -0,0 +1,40 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let intervalId;
+
+function startTimer() {
+  intervalId = setInterval(updateTimer, 1000);
+}
+
+function stopTimer() {
+  clearInterval(intervalId);
+}
+
+function resetTimer() {
+  clearInterval(intervalId);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  document.getElementById("timer").innerText = "00:00:00";
+}
+
+function updateTimer() {
+  seconds++;
+  if (seconds === 60) {
+    minutes++;
+    seconds = 0;
+  }
+  if (minutes === 60) {
+    hours++;
+    minutes = 0;
+  }
+  const formattedSeconds = seconds.toString().padStart(2, "0");
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+  const formattedHours = hours.toString().padStart(2, "0");
+  document.getElementById("timer").innerText = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+document.getElementById("start").addEventListener("click", startTimer);
+document.getElementById("stop").addEventListener("click", stopTimer);
+document.getElementById("reset").addEventListener("click", resetTimer);
